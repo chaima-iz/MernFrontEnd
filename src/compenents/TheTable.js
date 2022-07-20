916a78d56d1637db28a436a507ac2447a2359dc6
chaima-iz <shaymaeizwaghne.01@gmail.com>
2022-07-20 14:55:35 +0100

    commit

--- src/compenents/TheTable.js
+++ src/compenents/TheTable.js
@@ -51,7 +51,7 @@ export default function BasicTable() {
   };
   return (
     <FriendContext.Consumer>
-      {console.log(friend)}
+      {console.log("friend",friend)}
     {friend !== {} ? setListOfFriends([...listOfFriends, {name:"test",age:30}]):setListOfFriends(listOfFriends)}
       <TableContainer component={Paper}>
         <Table aria-label="simple table">
